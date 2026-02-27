e75092d6081c3a414934e366c246959fc745f1e9
pcode23dev <pcodedev23@gmail.com>
2026-02-27 18:18:10 +0100

    Add biblioteca fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
 import "./styles/global.css";
 import "./styles/tema.css";
 
-
-export default function App(){
-  return(
-    <h1>Olá, mundo!</h1>
-  )
-};
+export default function App() {
+  return (
+    <div className="grid grid-rows-[auto_1fr_auto] min-h-screen bg-bg-color text-text-color">
+      <div>
+        <nav className="hidden md:block">
+          <ul className="flex justify-around py-2">
+            <li>Historico</li>
+            <li>Home</li>
+            <li>Ajustes</li>
+            <li>Perfil</li>
+          </ul>
+        </nav>
+      </div>
+      <div id="headerMobile">
+        <h1 className="text-2xl font-bold text-center">Chronos Pombodouro</h1>
+      </div>
+      <div id="section">
+        <h2 className="text-xl font-semibold text-center">
+          Bem-vindo ao Chronos Pombodouro!
+        </h2>
+      </div>
+      <div id="footerMenuMobile" className="md:hidden">
+        <nav id="menuMobile" className="">
+          <ul className="flex justify-around py-2">
+            <li>Home</li>
+            <li>Historico</li>
+            <li>Ajustes</li>
+            <li>Perfil</li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
